303f76e8d28af07a2f2765d83a17f0331930b931
Joneyviana <joneyviana@gmail.com>
2020-05-26 10:27:55 -0300

    check download complete

---
+++ check_download_complete.py
@@ -0,0 +1,10 @@
+def every_downloads_chrome(driver):
+    if not driver.current_url.startswith("chrome://downloads"):
+        driver.get("chrome://downloads/")
+    return driver.execute_script("""
+        var items = document.querySelector('downloads-manager')
+        .shadowRoot.querySelector('#downloadsList')
+        .items;
+        if (items.every(e => e.state === "COMPLETE"))
+            return items.map(e => e.fileUrl || e.file_url);
+        """)
